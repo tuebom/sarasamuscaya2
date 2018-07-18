909b41f14d405ff646c13b75e51ec0e80cbff890
tuebom <tuebom.wirya@gmail.com>
2018-07-19 00:02:17 +0800

    Update

--- www/js/routes.js
+++ www/js/routes.js
@@ -56,6 +56,17 @@ routes = [
           // },
         // ]
       // };
+      
+      var swiper = app.swiper.create('.swiper-container', {
+          speed: 400,
+          //slidesPerView: auto,
+          loop: true,
+          //autoHeight: true,
+          shortSwipes: false,
+          longSwipes: false,
+          //effect:'fade'
+          //spaceBetween: 100
+      });        
 
       if (db) {
         db.transaction(function(tx) {
@@ -97,16 +108,6 @@ routes = [
             // app.dialog.alert('select error: ' + error.message);
           // });      
         // }
-        var swiper = app.swiper.create('.swiper-container', {
-            speed: 400,
-            //slidesPerView: auto,
-            loop: true,
-            //autoHeight: true,
-            shortSwipes: false,
-            longSwipes: false,
-            //effect:'fade'
-            //spaceBetween: 100
-        });        
       
         // to do
         $$('i.material-icons.fav').on('click', function (e) {//Changing color icons onclick
